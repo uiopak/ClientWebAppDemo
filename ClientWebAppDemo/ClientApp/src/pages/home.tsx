621c8c1bca8d270a463cd7e193a9cc79c5a692bf
uiopak <44174807+uiopak@users.noreply.github.com>
2022-12-07 01:59:29 +0100

    Add username and password to home and login page, test docker fix

--- ClientWebAppDemo/ClientApp/src/pages/home.tsx
+++ ClientWebAppDemo/ClientApp/src/pages/home.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     return (
         <section class="bg-base-100 text-base-700 p-4">
             <h1 class="text-2xl font-bold">Demo app:</h1>
+            <p class="mt-4">Contact - contact list</p>
+            <p class="mt-4">Login - login to edit, delete and add new contacts</p>
             <p class="mt-4">About - information about app</p>
         </section>
     );
--- ClientWebAppDemo/ClientApp/src/pages/login.tsx
+++ ClientWebAppDemo/ClientApp/src/pages/login.tsx
@@ -43,6 +43,18 @@ export default function Login() {
 
     return (
         <section class="bg-base-100 text-base-700 p-4">
+            <div class="alert alert-info shadow-lg m-2 p-2">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>User1: Login: admin Password: paS$w0rd</span>
+                </div>
+            </div>
+            <div class="alert alert-info shadow-lg m-2 p-2">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>User2: Login: demo  Password: paS$w0rd</span>
+                </div>
+            </div>
             <div class="card shadow-2xl bg-base-100">
                 <div class="card-body">
                     <div class="form-control">
